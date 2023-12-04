e8e733ccf6202b1f6d340029ce16387f7751e5eb
srj <1327928807@qq.com>
2023-12-04 10:18:37 +0800

    初始化

---
+++ src/main/java/tm/mc/groupingsystem/GroupingSystemApplication.java
@@ -0,0 +1,13 @@
+package tm.mc.groupingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tm/mc/groupingsystem/GroupingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tm.mc.groupingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
